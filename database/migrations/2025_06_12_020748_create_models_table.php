0a9293604a90d94ee72eb2ab50e0b3c945c30c17
deepalsr <deepalshrt@gmail.com>
2025-06-12 07:53:55 +0545

    feat: added models table;remove model table

--- database/migrations/2025_06_12_020748_create_models_table.php
+++ database/migrations/2025_06_12_020748_create_models_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('model', function (Blueprint $table) {
+        Schema::create('models', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('model');
+        Schema::dropIfExists('models');
     }
 };
